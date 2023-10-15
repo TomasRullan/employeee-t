624a53a2f29f1ce460f32e19fe542c3785bafd37
TomasRullan <tomasrullan@yahoo.com>
2023-10-15 15:25:30 -0400

    quary.sql

--- db/quary.sql
+++ db/quary.sql
@@ -1,2 +1,24 @@
 DROP DATABASE IF EXISTS employee-tracker_db;
 CREATE DATABASE employee-tracker_db;
+
+USE employee-tracker_db;
+
+CREATE TABLE department (
+    id INT PRIMARY KEY 
+    name VARCHAR(30)
+)
+
+CREATE TABLE role (
+    id INT PRIMARY KEY
+    title VARCHAR(30)
+    salary DECIMAL
+    department_id INT
+)
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY 
+    first_name VARCHAR(30)
+    last_name VARCHAR(30)
+    manager_id INT
+    null "if the employee has no manager" 
+)
