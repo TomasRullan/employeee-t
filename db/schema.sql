d28aa686d525582fea56362a458b000a7b75f701
TomasRullan <tomasrullan@yahoo.com>
2023-10-18 08:50:09 -0400

    schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,57 +1,26 @@
-DROP DATABASE IF EXISTS employees;
-CREATE DATABASE employees;
-USE employees;
-CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+DROP DATABASE IF EXISTS employeeTracker_db;
+CREATE DATABASE employeeTracker_db;
+USE employeeTracker_db;
+
+CREATE TABLE departments (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+department_name VARCHAR(255) NOT NULL
 );
-CREATE TABLE role (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) UNIQUE NOT NULL,
-  salary DECIMAL UNSIGNED NOT NULL,
-  department_id INT UNSIGNED NOT NULL,
-  INDEX dep_ind (department_id),
-  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(255),
+salary DECIMAL(10,2),
+department_id INT,
+FOREIGN KEY (department_id)
+REFERENCES departments(id)
+ON DELETE SET NULL
 );
+
 CREATE TABLE employee (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT UNSIGNED NOT NULL,
-  INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT NOT NULL
 );
-use employees;
-INSERT INTO department
-    (name)
-VALUES
-    ('Operations'),
-    ('Analystics'),
-    ('Marketing'),
-    ('Executive');
-INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('General Manager', 11000000, 1),
-    ('Coach', 4000000, 1),
-    ('Team Lead Analyst', 15000000, 2),
-    ('Team Analyst', 8000000, 2),
-    ('Media Manager', 7000000, 3),
-    ('Media Specialist', 3000000, 3),
-    ('CEO', 45000000, 4),
-    ('CEO Assistant', 25000000, 4);
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-VALUES
-    ('John', 'Stockton', 1, NULL),
-    ('Karl', 'Malone', 2, 1),
-    ('Michael', 'Jordan', 3, NULL),
-    ('Steve', 'Kerr', 4, 3),
-    ('Shawn', 'Kemp', 5, NULL),
-    ('Gary', 'Peyton', 6, 5),
-    ('Magic', 'Johnson', 7, NULL),
-    ('Larry', 'Bird', 8, 7);
-    
