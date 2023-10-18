268c962b5e61eb186ce4a9290c646f4e95e0d356
TomasRullan <tomasrullan@yahoo.com>
2023-10-18 08:51:47 -0400

    seeds.sql fix

--- db/seed.sql
+++ db/seed.sql
@@ -1,33 +1,38 @@
-SELECT role.id, role.title, role.salary FROM role ORDER BY role.id;
-SELECT role.id, role.title FROM role ORDER BY role.id;
-SELECT * FROM employee;
+INSERT INTO departments (department_name)
+VALUES 
+('Executive Board'),
+('Marketing'),
+('Human Resources'),
+('Finance'),
+('Engineering'),
+('Information Technology'),
+('Customer Relations'),
+('Research and Development'),
+('Legal'),
+('Maintenance');
 
-SELECT department.id, department.name FROM department ORDER BY department.id;
+INSERT INTO roles (title, salary, department_id)
+VALUES 
+('Chief Executive Officer', 555000.00, 1),
+('Marketing Manager', 125000.00, 2),
+('HR Director', 189000.00, 3),
+('Finance Head', 145000.00, 4),
+('Senior Engineer', 185000.00, 5),
+('IT Manager', 125000.00, 6),
+('Customer Relations Manager', 75000.00, 7),
+('Research and Development Manager ', 185000.00, 8),
+('Legal Manager', 95000.00, 9),
+('Maintenance Manager', 135000.00, 10);
 
-SELECT department.name AS department, role.title, employee.id, employee.first_name, employee.last_name
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY department.name;
-    
-SELECT CONCAT(manager.first_name, ' ', manager.last_name) AS manager, department.name AS department, employee.id, employee.first_name, employee.last_name, role.title
-  FROM employee
-  LEFT JOIN employee manager on manager.id = employee.manager_id
-  INNER JOIN role ON (role.id = employee.role_id && employee.manager_id != 'NULL')
-  INNER JOIN department ON (department.id = role.department_id)
-  ORDER BY manager;
-  
-SELECT role.title, employee.id, employee.first_name, employee.last_name, department.name AS department
-    FROM employee
-    LEFT JOIN role ON (role.id = employee.role_id)
-    LEFT JOIN department ON (department.id = role.department_id)
-    ORDER BY role.title;
-
-SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager
-  FROM employee
-  LEFT JOIN employee manager on manager.id = employee.manager_id
-  INNER JOIN role ON (role.id = employee.role_id)
-  INNER JOIN department ON (department.id = role.department_id)
-  ORDER BY employee.id;
-  
-SELECT first_name, last_name, role_id FROM employee 	WHERE employee.id = 4;
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Tom', 'Brady', 1, 1),
+('Thomas', 'The Tank Engine', 2, 2),
+('Thomas', 'Calle', 3, 3),
+('Thomas', 'Shelby', 4, 4),
+('Thomas', 'Hobbes', 5, 5),
+('Tom', 'Hanks', 6, 6),
+('Tom', 'Holland', 7, 7),
+('Tom', 'Ford', 8, 8),
+('Thomas', 'The Apostle', 9, 9),
+('Tom', 'Cruise', 10, 10);
