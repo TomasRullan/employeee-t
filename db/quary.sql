0009b07f405dcca4b656f850161012bd34cd08b3
TomasRullan <tomasrullan@yahoo.com>
2023-10-18 08:54:58 -0400

    fixing sql

--- db/quary.sql
+++ db/quary.sql
@@ -1,24 +1,5 @@
-DROP DATABASE IF EXISTS employee-tracker_db;
-CREATE DATABASE employee-tracker_db;
-
-USE employee-tracker_db;
-
-CREATE TABLE department (
-    id INT PRIMARY KEY 
-    name VARCHAR(30)
-)
-
-CREATE TABLE role (
-    id INT PRIMARY KEY
-    title VARCHAR(30)
-    salary DECIMAL
-    department_id INT
-)
-
-CREATE TABLE employee (
-    id INT PRIMARY KEY 
-    first_name VARCHAR(30)
-    last_name VARCHAR(30)
-    manager_id INT
-    null "if the employee has no manager" 
-)
+-- SELECT depaartment.department_name AS department, reviews.review
+-- FROM 
+-- LEFT JOIN department
+-- ON reviews.department_id = department.id
+-- ORDER BY departments.department_name;
